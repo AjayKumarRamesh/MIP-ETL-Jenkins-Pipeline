763a418ea24b0faa8d1ea962f74a4bee9e150648
Timothy Figgins <Timothy.Figgins1@ibm.com>
2022-04-27 16:43:03 -0400

    finished mapping for COP_to_MIP

--- jenkins_files/test/JenkinsFunctions.groovy
+++ jenkins_files/test/JenkinsFunctions.groovy
@@ -10,7 +10,7 @@ dagstoCOS = [RUBY_TO_MIP:['Ruby', 'RubyToMIP', 'digikeystore.jks', 'javacerts.jk
             MIP_SPSS_SCORING:['.', '.', 'digikeystore.jks', 'javacerts.jks'],
             ADHOC_MKTO_LEADXREF:['Adhoc_xref', '.', 'Universal-trustore.jks', 'digikeystore.jks', 'marketo_sftp_pem.pem', 'marketo_sftp_prod.pem'],
             GRP_EVENTS_IDM:['.', '.', 'digikeystore.jks', 'javacerts.jks'],
-            CMDP_COP_to_MIP:['cmdp_cop_image', 'cmdp_cop_jar']]
+            CMDP_COP_to_MIP:['.', 'AccountIngestion', 'digikeystore.jks', 'javacerts.jks']]
 // image, jar 
 airflow = [RUBY_TO_MIP:['ruby_image', 'ruby_app_jar'],
            CDSExtract:['cds_image', 'cds_app_jar'],
